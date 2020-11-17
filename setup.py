bfd5e4510ee5db40386cc56467d04371422006ba
David Morrison <dm236@st-andrews.ac.uk>
2020-11-17 16:08:29 +0000

    Add repath package directory and configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='repath',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Reproducing Deep Learning in Digital Pathology',
+    author='iCAIRD'
+)
