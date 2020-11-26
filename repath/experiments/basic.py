bfa7d78e11acbb5edc6bc1f00a3357f273f0c33e
David Morrison <dm236@st-andrews.ac.uk>
2020-11-26 16:10:55 +0000

    Add stub for experiments package

--- repath/experiments/basic.py
+++ repath/experiments/basic.py
@@ -1,2 +1,17 @@
+# how should the directory structure be setup?
+#
+
+
 def preprocessing() -> None:
-    
+    # define the background subtractor#
+    # load the dataset, passing in the background subtractor
+    pass
+
+
+def patch_training() -> None:
+    pass
+
+
+def postprocessing() -> None:
+    pass
+
