ca9f5c50a708d4f4787933d67f3c9e3c55b68baf
David Morrison <dm236@st-andrews.ac.uk>
2020-11-27 16:04:47 +0000

    SImplify Dataset class

--- repath/data/datasets/camelyon16.py
+++ repath/data/datasets/camelyon16.py
@@ -4,25 +4,25 @@ from typing import Dict, Tuple
 import pandas as pd
 from repath.data.annotations.annotation import AnnotationSet
 from repath.data.annotations.asapxml import load_annotations
-from repath.data.datasets.dataset import AnnotationSetLoader, DatasetInfoABC
+from repath.data.datasets.dataset import AnnotationSetLoader, Dataset
 from repath.data.slides.openslide import Slide
-from repath.data.slides.slide_abc import SlideABC
+from repath.data.slides.slide import SlideBase
+from repath.utils.paths import project_root
 
 
-class Camelyon16(DatasetInfoABC):
-    @property
-    def paths(self, train_or_test: str) -> pd.DataFrame:
-        raise NotImplementedError
+class Camelyon16(Dataset):
+    def __init__(self, root: Path, paths: pd.DataFrame) -> None:
+        super().__init__(root, paths)
 
-    @property
-    def loaders(self) -> Tuple[AnnotationSetLoader, SlideABC]:
+    def load_annotations(self, file: Path) -> AnnotationSet:
+        # if there is no annotation file the just pass and empty list
+        annotations = load_annotations(file) if file else []
         labels_order = ["background", "tumor", "normal"]
+        return AnnotationSet(annotations, self.labels, labels_order, "normal")
 
-        def loader(file: Path) -> AnnotationSet:
-            annotations = load_annotations(file)
-            return AnnotationSet(annotations, self.labels, labels_order, "normal")
-
-        return loader, Slide
+    @property
+    def slide_cls(self) -> SlideBase:
+        return Slide
 
     @property
     def labels(self) -> Dict[str, int]:
@@ -30,7 +30,23 @@ class Camelyon16(DatasetInfoABC):
 
 
 def training():
-    return Camelyon16("train")
+    root = project_root() / "data" / "camelyon16" / "training"
+
+    annotations_dir = root / "lesion_annotations"
+    tumor_slide_dir = root / "tumor"
+    normal_slide_dir = root / "normal"
+
+    # path in the paths data frame are relative to the dataset 'root'
+    annotation_paths = [p.relative_to(root) for p in annotations_dir.glob("*.xml")]
+    tumor_slide_paths = [p.relative_to(root) for p in tumor_slide_dir.glob("*.tif")]
+    normal_slide_paths = [p.relative_to(root) for p in normal_slide_dir.glob("*.tif")]
+
+    # turn them into a data frame
+    df = pd.DataFrame()
+    df["slide"] = tumor_slide_paths + normal_slide_paths
+    df["annotation"] = annotation_paths  # TODO: check this works - on the DGX!
+
+    return Camelyon16(root, df)
 
 
 def testing():
--- repath/data/datasets/dataset.py
+++ repath/data/datasets/dataset.py
@@ -1,69 +1,50 @@
 from abc import ABCMeta, abstractmethod
+from collections import Sequence
 from pathlib import Path
-from repath.data.slides.openslide import Slide
-from repath.data.annotations.asapxml import load_annotations
-from typing import Callable, List, Tuple
+from repath.data.slides.slide import SlideBase
+from typing import Callable, Dict, Tuple
 
 import pandas as pd
-from repath.data.annotations.annotation import Annotation, AnnotationSet
-from repath.data.slides.slide_abc import SlideABC
+from repath.data.annotations.annotation import AnnotationSet
 
 AnnotationSetLoader = Callable[[Path], AnnotationSet]
 
 
-class DatasetInfoABC(metaclass=ABCMeta):
-    @property
+class Dataset(Sequence, metaclass=ABCMeta):
+    """ A data set is an object that represents a set of slides and their annotations.
+
+    It can be used to load and iterate over a set of slides and their annotations.
+    This is an abstract base class where classes that represent specific data sets
+    should overload the load_annotations and slide_cls methods. See their descriptions
+    for details.
+    It implements the Sequence protocol so that it can be iterated over.
+
+    Args:
+        Sequence ([type]): [description]
+        metaclass ([type], optional): [description]. Defaults to ABCMeta.
+    """
+
+    def __init__(self, root: Path, paths: pd.DataFrame) -> None:
+        # process the paths_df (has two columns 'slide', 'annotation')
+        self.root = root
+        self.paths = paths
+
     @abstractmethod
-    def paths(self) -> pd.DataFrame:
+    def load_annotations(file: Path) -> AnnotationSet:
         raise NotImplementedError
 
     @property
     @abstractmethod
-    def annotation_loader(self) -> Tuple[AnnotationSetLoader, SlideABC]:
+    def slide_cls(self) -> SlideBase:
         raise NotImplementedError
 
     @property
     def labels(self) -> Dict[str, int]:
         raise NotImplementedError
 
+    def __len__(self):
+        return len(self.paths)
 
-class Dataset:
-    """Container class for all of the slides and information about a dataset
-    How do we use this class?
-    - Transform it by iterating over it!
-    - So it's a sequence?
-    """
-
-    def __init__(self, info: DatasetInfoABC) -> None:
-        # process the paths_df (has two columns 'slide', 'annotation')
-        self.info = info
-        self.wsis = []  # TODO: refactor to a different name. wsis is confusing.
-
-    def load(self) -> None:
-        load_annots, Slide = self.info.loaders
-        # TODO: line below doesn't work for slides with multiple images
-        self.wsis = [
-            (Slide(row.slide), load_annots(row.annotation))
-            for row in self.paths.itertuples()
-        ]
-
-    def make_patches(
-        self,
-        labels_level: int,
-        patch_finder: PatchFinder,
-        tissue_detector: TissueDetector,
-    ) -> None:
-        load_annots, Slide = self.info.loaders
-        for wsi in self.wsis:
-            with Slide(wsi.slide) as slide:
-                for image in slide.images:
-                    thumb = image.get_thumbnail(labels_level)
-                    scale_factor = 2 ** labels_level
-                    labels_image = wsi
-
-            # 1. create the tissue mask
-
-            # 2.
-
-    def save_patches(self, path: Path) -> None:
-        pass
+    def __getitem__(self, idx):
+        row = self.paths.iloc[idx]
+        return row["slide"], row["annotation"]
