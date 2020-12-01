86962427703d49d41ad0d1ef40f58c4f3657f638
David Morrison <david@nv-dgx-1.st-andrews.ac.uk>
2020-12-01 22:26:40 +0000

    Add converter methods to Size and Shape

--- repath/utils/geometry.py
+++ repath/utils/geometry.py
@@ -20,7 +20,13 @@ class Size(NamedTuple):
     width: int
     height: int
 
+    def as_shape(self):
+        return Shape(self.height, self.width)
+
 
 class Shape(NamedTuple):
     num_rows: int
     num_cols: int
+
+    def as_size(self):
+        return Size(self.num_cols, self.num_rows)
