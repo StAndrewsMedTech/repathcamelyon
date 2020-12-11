8ba1a186d1202aa9d37524d109c035f63650f3ce
David Morrison <david@nv-dgx-1.st-andrews.ac.uk>
2020-12-11 18:43:49 +0000

    Simplify splitter and sampler

--- repath/preprocess/sampling/sampler.py
+++ repath/preprocess/sampling/sampler.py
@@ -3,20 +3,5 @@ from typing import Tuple
 
 from repath.preprocess.patching.patch_index import PatchIndex
 
-class Sampler(metaclass=ABCMeta):
-    @abstractmethod
-    def __call__(self, patches: PatchIndex, num_samples: int) -> PatchIndex:
-        raise NotImplementedError
-
-
-class StratifiedPatchSampler(Sampler):
-    """This class performs a stratified sampling over all slides in a patches 
-    index in order to balance the amount of patches with different labels in the data.
-    
-    It samples from each slide based on the number of patches.
-    """
-    def __init__(self) -> None:
-        pass
-
-    def __call__(self, patches: PatchIndex, num_samples: int):
-        pass
+def sample(index: PatchIndex, num_samples: int) -> PatchIndex:
+    pass
